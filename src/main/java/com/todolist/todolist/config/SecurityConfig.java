e3ea64ac1c97af7101ca1370f9b08885912b3931
82everywin <82everywin@gmail.com>
2024-11-04 19:01:13 +0900

    서버 재푸시

---
+++ src/main/java/com/todolist/todolist/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.todolist.todolist.config;
+
+import com.todolist.todolist.security.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig  {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                // CSRF 토큰 없이 요청 처리
+                .csrf((csrfConfig) ->
+                        csrfConfig.disable())
+                .cors((corsConfig) ->
+                        corsConfig.disable())
+                .sessionManagement((sessionManagement) ->
+                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests((authorizeRequests) ->
+                        authorizeRequests
+                                .requestMatchers("/","/swagger-ui/**","/v3/api-docs/**").permitAll()
+                                .requestMatchers("/api/members","api/members/login").permitAll()
+                                .anyRequest().authenticated())
+                .addFilterBefore(new JwtTokenFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+
+}
